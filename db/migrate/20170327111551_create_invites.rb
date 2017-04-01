3e350446e8634c59e2322cfff4bdca9523f9e3a2
sense6 <konrad.worobiej@gmail.com>
2017-04-01 18:17:34 +0200

    migration fix

--- db/migrate/20170327111551_create_invites.rb
+++ db/migrate/20170327111551_create_invites.rb
@@ -1,7 +1,7 @@
 class CreateInvites < ActiveRecord::Migration[5.0]
   def change
     create_table :invites do |t|
-      t.index [:sender_id, :recever_id], unique: true
+      t.index :sender_id, :recever_id
 
       t.timestamps
     end
