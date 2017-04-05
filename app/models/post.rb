353bd80775c7eaf2866c7eca4eeac05a879d7ab2
sense6 <konrad.worobiej@gmail.com>
2017-04-05 14:39:25 +0200

    heroku fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"],
   file_name: { matches: [/png\z/, /jpe?g\z/, /gif\z/] },
   size: { less_than: 2.megabytes }
 
